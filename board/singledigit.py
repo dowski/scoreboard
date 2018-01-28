4aaa0d015af36f61529da2cf303f3ab55705b5ad
Christian Wyglendowski <christian@dowski.com>
2018-01-28 13:47:35 -0500

    Fix a fundamental issue in bit shifting
    I was apparently doing latching and clocking in the wrong order. This manifest
    itself when I was trying to use the SingleDigitDisplay to set a single solid
    digit (without pulsing). It was briefly lighting up the right segments and
    then lighting them all.
    
    Doing the latching and clocking in this new order seems to have fixed the
    issue.

--- board/singledigit.py
+++ board/singledigit.py
@@ -13,30 +13,23 @@ class SingleDigitDisplay(object):
 
     def set(self, n):
         value = control.DIGITS[n]
+        control.latch.off()
         for bit in value:
             if bit:
                 control.data.on()
             else:
                 control.data.off()
-            control.clk.on()
             control.clk.off()
+            control.clk.on()
         control.latch.on()
-        control.latch.off()
 
 if __name__ == '__main__':
     import control
     import time
 
-    left = SingleDigitDisplay(control.left_display)
-    right = SingleDigitDisplay(control.right_display)
-
+    display = SingleDigitDisplay(control.right_display)
+    display.enable()
     while True:
-        left.set(4)
-        left.enable()
-        time.sleep(.01)
-        left.disable()
-        right.set(2)
-        right.enable()
-        time.sleep(.01)
-        right.disable()
-
+        for i in xrange(10):
+            display.set(i)
+            time.sleep(1)
