ef7b7fc14553f28b2841b5d6993fe8b4f7c3ae1c
Christian Wyglendowski <christian@dowski.com>
2018-02-11 14:49:00 -0500

    Update the display for two scores and innings

--- board/display.py
+++ board/display.py
@@ -6,17 +6,22 @@ from multiplexor import Multiplexor
 from shiftregister import ShiftRegister
 
 
+DISPLAY_COUNT = 3
+
+
 class DisplayController(object):
     """A threaded interface to a display composed of 7-segment displays.
 
     """
     def __init__(self):
         self.commands = Queue.Queue()
-        shift_register = ShiftRegister(control.data, control.clk, control.latch)
+        shift_register = ShiftRegister(
+                control.data, control.clk, control.latch)
         self.multiplexor = Multiplexor(
-                2, shift_register, control.left_display, control.right_display)
+                DISPLAY_COUNT, shift_register, control.left_display,
+                control.right_display)
         self._render_thread = threading.Thread(target = self._mainloop)
-        self.values = [None, None]
+        self.values = [None] * DISPLAY_COUNT
 
     def on(self):
         self._render_thread.start()
@@ -26,14 +31,18 @@ class DisplayController(object):
         self._render_thread.join()
 
     def clear_all(self):
-        self.values = [None, None]
+        self.values = [None] * DISPLAY_COUNT
         self.commands.put(('set', list(self.values)))
 
-    def set_left(self, value):
+    def set_inning(self, value):
+        self.values[2] = value
+        self.commands.put(('set', list(self.values)))
+
+    def set_top_score(self, value):
         self.values[0] = value
         self.commands.put(('set', list(self.values)))
 
-    def set_right(self, value):
+    def set_bottom_score(self, value):
         self.values[1] = value
         self.commands.put(('set', list(self.values)))
 
@@ -52,13 +61,19 @@ class DisplayController(object):
 
 if __name__ == '__main__':
     import time
+    from itertools import chain
     dc = DisplayController()
     dc.on()
     try:
-        for a,b in zip(range(100), reversed(range(100))):
-            dc.set_left(a)
-            dc.set_right(b)
-            time.sleep(0.1)
+        while True:
+            for a,b,c in zip(
+                    range(100),
+                    reversed(range(100)),
+                    chain(*zip(range(0,100,2), range(0,100,2)))):
+                dc.set_top_score(a)
+                dc.set_bottom_score(b)
+                dc.set_inning(c)
+                time.sleep(0.1)
     finally:
         dc.off()
 
--- displaysample.py
+++ displaysample.py
@@ -44,8 +44,9 @@ def main(argv):
         display.off()
 
 def show_runs(display, indians, opponent):
-    display.set_left(indians)
-    display.set_right(opponent)
+    display.set_top_score(indians)
+    display.set_bottom_score(opponent)
+    display.set_inning(9)
     time.sleep(2)
     display.clear_all()
     time.sleep(.5)
