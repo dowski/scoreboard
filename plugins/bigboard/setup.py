8d12a6f664fdbb57f1641cba716f76a97d3c8703
Christian Wyglendowski <christian@dowski.com>
2020-08-01 19:50:50 -0400

    Start moving to a plugin system for displays

---
+++ plugins/bigboard/setup.py
@@ -0,0 +1,72 @@
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'scoreboard.plugins.bigboard'
+DESCRIPTION = 'Source code that powers my big scoreboard'
+URL = 'https://github.com/dowski/scoreboard'
+EMAIL = 'christian@dowski.com'
+AUTHOR = 'Christian Wyglendowski'
+REQUIRES_PYTHON = '>=3.5.0, <4.0.0'
+VERSION = '0.9.0'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'luma.led_matrix == 1.4.1',
+]
+
+REQUIRED_FOR_TESTS = [
+    'pytest',
+    'mock',
+]
+
+REQUIRED_FOR_SETUP = [
+    'pytest-runner',
+]
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+#with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+#    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    #long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    entry_points={
+        'scoreboard.display.plugins': [
+            'bigboard=bigboard.display:DisplayController'],
+    },
+    install_requires=REQUIRED,
+    setup_requires=REQUIRED_FOR_SETUP,
+    tests_require=REQUIRED_FOR_TESTS,
+    include_package_data=True,
+    license='BSD',
+)
